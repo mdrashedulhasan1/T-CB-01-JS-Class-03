fd0d6929aa5ee111af9746fa6efff886ff9508e0
Rashedul Hasan <Rashedul Hasan>
2022-03-24 04:46:40 +0600

    JS-Class-03

---
+++ index.js
@@ -0,0 +1,61 @@
+// var friendsAge = [12, 23, 34, 45, 56];
+// friendsAge[1] = 22;
+// var position = friendsAge.indexOf(22);
+// var rakib = friendsAge[1];
+// console.log(position);
+
+// var friendList = ['rakib', 'sarowal'];
+// friendList.push('tapu');
+// friendList.pop();
+// friendList.unshift('tapu');
+// friendList.shift();
+// console.log(friendList);
+
+// var num = 1;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+// for(var i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// var num = 10;
+// switch(num){
+//     case 1:
+//         console.log("One");
+//         break;
+//     case 2:
+//         console.log("Two");
+//         break;
+//     case 3:
+//         console.log("Three");
+//         break;
+//     case 4:
+//         console.log("Four");
+//         break;
+//     case 5:
+//         console.log("Five");
+//         break;
+//     default:
+//         console.log("Number not found!");
+
+// }
+
+function personInfo(){
+    console.log("Name:Tapu");
+    console.log("City:Tangail");
+}
+personInfo()
+personInfo()
+
+function add(num1, num2){
+  return num1+num2;
+}
+var total = add(2, 3);
+var total2 = add(5, 3);
+console.log(total);
+console.log(total2);
+personInfo()
+
